e5d93c85666ae0ff12202a3b37ef32fa3fbc4337
Matheus-NUC <matheus.doer@gmail.com>
2024-09-06 19:09:44 -0300

    Initial Commit

---
+++ lib/src/app_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:listinha/src/home/home_page.dart';
+import 'package:listinha/src/shared/theme/theme.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      home: const HomePage(),
+    );
+  }
+}
