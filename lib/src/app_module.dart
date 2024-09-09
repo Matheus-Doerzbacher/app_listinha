52f0dfb16ceb65ace934a137b8d0277f3810a136
Matheus-NUC <matheus.doer@gmail.com>
2024-09-09 09:04:37 -0300

    criado um estado global

--- lib/src/app_module.dart
+++ lib/src/app_module.dart
@@ -1,8 +1,14 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:listinha/src/configuration/configuration_page.dart';
 import 'package:listinha/src/home/home_module.dart';
+import 'package:listinha/src/shared/stores/app_store.dart';
 
 class AppModule extends Module {
+  @override
+  void binds(Injector i) {
+    i.addSingleton(AppStore.new);
+  }
+
   @override
   void routes(RouteManager r) {
     r
--- lib/src/configuration/configuration_page.dart
+++ lib/src/configuration/configuration_page.dart
@@ -1,21 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:listinha/src/shared/stores/app_store.dart';
 
-class ConfigurationPage extends StatefulWidget {
+class ConfigurationPage extends StatelessWidget {
   const ConfigurationPage({super.key});
 
   @override
-  State<ConfigurationPage> createState() => _ConfigurationPageState();
-}
-
-class _ConfigurationPageState extends State<ConfigurationPage> {
-  @override
   Widget build(BuildContext context) {
+    final appStore = context.watch<AppStore>(
+      (bind) => bind.themeMode,
+    );
     return Scaffold(
       appBar: AppBar(
         title: const Text('Listinha'),
       ),
-      body: const Center(
-        child: Text('Pagina de Configuração'),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Configuração',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'Tema',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              const SizedBox(height: 10),
+              RadioListTile(
+                value: ThemeMode.system,
+                groupValue: appStore.themeMode.value,
+                title: const Text('Sistema'),
+                onChanged: appStore.changeThemeMode,
+              ),
+              RadioListTile(
+                value: ThemeMode.light,
+                groupValue: appStore.themeMode.value,
+                title: const Text('Claro'),
+                onChanged: appStore.changeThemeMode,
+              ),
+              RadioListTile(
+                value: ThemeMode.dark,
+                groupValue: appStore.themeMode.value,
+                title: const Text('Escuro'),
+                onChanged: appStore.changeThemeMode,
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'Controle de dados',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              const SizedBox(height: 10),
+              OutlinedButton(
+                onPressed: () {},
+                child: const Text('Apagar cache e reinicar o app'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
