f49d657718bd8ac126e55234f77003ec28a1548e
Matheus-NUC <matheus.doer@gmail.com>
2024-09-09 13:22:43 -0300

    criado os models para o realm

--- lib/src/app_module.dart
+++ lib/src/app_module.dart
@@ -1,12 +1,18 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:listinha/src/configuration/configuration_page.dart';
 import 'package:listinha/src/home/home_module.dart';
+import 'package:listinha/src/shared/service/realm/models/configuration/service/configuration_service.dart';
+import 'package:listinha/src/shared/service/realm/realm_config.dart';
 import 'package:listinha/src/shared/stores/app_store.dart';
+import 'package:realm/realm.dart';
 
 class AppModule extends Module {
   @override
   void binds(Injector i) {
-    i.addSingleton(AppStore.new);
+    i
+      ..addInstance<Realm>(Realm(config))
+      ..add<ConfigurationService>(ConfigurationServiceImpl.new)
+      ..addSingleton(AppStore.new);
   }
 
   @override
--- lib/src/shared/stores/app_store.dart
+++ lib/src/shared/stores/app_store.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:listinha/src/shared/service/realm/models/configuration/service/configuration_service.dart';
 
 class AppStore {
   // THEME
   final themeMode = ValueNotifier(ThemeMode.system);
+  final syncDate = ValueNotifier<DateTime?>(null);
+
+  final ConfigurationService _configurationService;
+
+  AppStore(this._configurationService) {
+    init();
+  }
+
+  void init() {
+    final model = _configurationService.getConfiguration();
+    syncDate.value = model.syncDate;
+    themeMode.value = getThemeModeByName(model.themeModeName);
+  }
+
+  void save() {
+    _configurationService.saveConfiguration(
+      themeMode.value.name,
+      syncDate.value,
+    );
+  }
 
   void changeThemeMode(ThemeMode? mode) {
     if (mode != null) {
@@ -11,19 +32,16 @@ class AppStore {
     }
   }
 
-  // SYNC DATE
-  final syncDate = ValueNotifier<DateTime?>(null);
-
   void setSyncDate(DateTime date) {
     syncDate.value = date;
     save();
   }
 
-  void save() {
-    // Todo: Sa;var os dados na base local
+  void deleteApp() {
+    _configurationService.deleteAll();
   }
 
-  void init() {
-    // Todo: Sa;var os dados na base local
+  ThemeMode getThemeModeByName(String themeName) {
+    return ThemeMode.values.firstWhere((mode) => mode.name == themeName);
   }
 }
